e5ebb4e8d5b1e94cbf983723df6d5303abd6c3b5
Margaret Rideout <margaretpardy@gmail.com>
2026-04-27 12:21:40 +0000

    Manual code change

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Garden from './pages/Garden';
 
-// We strip away the complex wrappers to ensure the Garden renders immediately
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* This loads your Garden.jsx file directly */}
-        <Route path="/" element={<Garden />} />
-        
-        {/* If any other route is hit, show the Garden */}
-        <Route path="*" element={<Garden />} />
-      </Routes>
-    </Router>
-  );
+export default function App() {
+  return <Garden />;
 }
-
-export default App;
